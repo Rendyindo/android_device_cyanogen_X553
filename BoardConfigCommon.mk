335ebfdff74f18370a25f95e20af6bd95654eebb
Rendyindo <rendyarya22@gmail.com>
2017-01-13 16:41:23 +0700

    Some X553 things

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VENDOR_PATH := device/cyanogen/msm8916-common
+VENDOR_PATH := device/Infinix/X553
 
 # Include board config fragments
 include $(VENDOR_PATH)/board/*.mk
